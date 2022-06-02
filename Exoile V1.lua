59da9b565e7ac43eecbba12cc7f0cf6962a6823a
idk <74161148+heywhyyoulookinghere@users.noreply.github.com>
2022-06-02 19:56:27 +0100

    Update Exoile V1.lua

--- Exoile V1.lua
+++ Exoile V1.lua
@@ -365,9 +365,9 @@ sendnotif("Exoile V1 loaded.")
 
 game.Players.PlayerAdded:Connect(function(player)
 if chatplayerjoins == true then
-   chat(player.Name.." has joined the server!")
+   command("h "..player.DisplayName.."("..player.Name..") has joined the server!")
 end
 if chatplayerjoins == false then
-   sendnotif(player.Name.." has joined the server!")
+   sendnotif(player.DisplayName.."("..player.Name..") has joined the server!")
 end
 end)
